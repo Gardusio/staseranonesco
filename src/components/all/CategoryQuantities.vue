5d3eb6f36e50dd15312005fb7f640cffa1750fb6
Gardusio <fabioinnabarbieri@gmail.com>
2021-06-02 22:02:32 +0200

    WIP

--- src/components/all/CategoryQuantities.vue
+++ src/components/all/CategoryQuantities.vue
@@ -24,40 +24,22 @@ export default {
     };
   },
   created() {
-    const orders = this.$store.getters["orders/getOrders"];
     const takeaways = this.$store.getters["takeaways/getTakeAways"];
     const deliveries = this.$store.getters["deliveries/getDeliveries"];
 
-    console.log(
-      "created: ",
-      orders.length,
-      takeaways.length,
-      deliveries.length
-    );
-
     let lineItems = [];
-    for (let k = 0; k < orders.length; k = k + 1) {
-      lineItems = [...orders[k].lineItems];
-    }
-    console.log(lineItems.length);
-    for (let i = 0; i < takeaways.length; i = i + 1) {
-      lineItems = [...takeaways[i].lineItems];
-    }
-    console.log(lineItems.length);
+     console.log(new Date(this.lower));
 
-    // let allOrders = [...orders, ...takeaways, ...deliveries];
-    // console.log(allOrders);
-    // // let lineItems = [];
-    // for (let i = 0; i < allOrders.length; i++) {
-    //   for (let j = 0; allOrders[i].lineItems.length; j++) {
-    //     console.log(allOrders[i].lineItems[j]);
-    //   }
-    // }
-    // this.lineItems = lineItems;
-    // console.log(this.lineItems);
+    for (let i = 0; i < takeaways.length; i = i + 1) {
+      console.log(takeaways[i].hour);
+      lineItems = [...lineItems, ...takeaways[i].lineItems];
+    }
+    for (let j = 0; j < deliveries.length; j = j + 1) {
+      console.log(deliveries[j].hour)
+      lineItems = [...lineItems, ...deliveries[j].lineItems];
+    }
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
